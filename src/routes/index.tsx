caf8ab5a26282857bb08de30cd9d7b133f6b75c4
HRH King Will Blanch Asa Yasharahala <blanchgroupinfo@gmail.com>
2026-06-22 21:40:56 +0300

    update

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -430,11 +430,6 @@ function Index() {
           <i />
         </div>
       </section>
-
-      {/* Slideshow of images and videos from src/assets/slideshow */}
-      <section id="slideshow" className="section-shell py-12 bg-background">
-        <Slideshow />
-      </section>
       <ScriptureBand
         text="But ye are a chosen generation, a royal priesthood, an holy nation, a peculiar people; that ye should shew forth the praises of him who hath called you out of darkness into his marvellous light:"
         verse="1 Peter 2:9"
@@ -449,6 +444,10 @@ function Index() {
             <em>of Judah (Yadah)</em>
           </h2>
             {/* Slideshow of images and videos placed after the scripture */}
+                  {/* Slideshow of images and videos from src/assets/slideshow */}
+      <section id="slideshow" className="section-shell py-12 bg-background">
+        <Slideshow />
+      </section>
               <div className="my-8"><Slideshow /></div>
         </div>
         <div className="heritage-grid">

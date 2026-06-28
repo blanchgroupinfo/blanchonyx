daf6dda492528204e38bbfe0e743a256cc3ae060
HRH King Will Blanch Asa Yasharahala <blanchgroupinfo@gmail.com>
2026-06-28 18:23:44 +0300

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite';
 import path from 'path';
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [TanStackRouterVite(), react()],
   resolve: {
     alias: {
       // Maintains the '@' alias pointing to the src directory
@@ -19,6 +20,9 @@ export default defineConfig({
     sourcemap: false,
   },
   server: {
+    host: '0.0.0.0',
     port: 3000,
+    hmr: process.env.DISABLE_HMR !== 'true',
+    watch: process.env.DISABLE_HMR === 'true' ? null : {},
   },
 });
